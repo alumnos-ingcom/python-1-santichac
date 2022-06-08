e2bf2fa7187982595430cbe67a1d0c71e8323593
Santi Chac <santichac3@gmail.com>
2022-06-08 12:30:55 -0300

    Agregue test_ejercicio2.py

---
+++ tests/test_ejercicio2.py
@@ -0,0 +1,35 @@
+from src.ejercicio2 import signo
+
+"""
+Este es el test correspondiente al archivo 'ejercicio1.py'
+"""
+
+def test_signo_positivo():
+    """
+    Este test evalua la función signo() ingresando un valor 
+    revisando que el resultado sea el numero esperado
+    """
+    numero = 9.3
+    resultado = signo(numero)
+    assert isinstance(resultado, float), 'El resultado debe ser de tipo float o sea, que sea un número con coma.'
+    assert resultado == 'Es positivo', 'El resultado no es el esperado'
+
+def test_signo_negativo():
+    """
+    Este test evalua la función signo() ingresando un valor 
+    revisando que el resultado sea el numero esperado
+    """
+    numero = -9.3
+    resultado = signo(numero)
+    assert isinstance(resultado, float), 'El resultado debe ser de tipo float o sea, que sea un número con coma.'
+    assert resultado == 'Es negativo', 'El resultado no es el esperado.'
+
+def test_signo_cero():
+    """
+    Este test evalua la función signo() ingresando un valor 
+    revisando que el resultado sea el numero esperado
+    """
+    numero = 0
+    resultado = signo(numero)
+    assert isinstance(resultado, int),  'El resultado debe ser de tipo float o sea, que sea un número con coma.'
+    assert resultado == 'Es cero', 'El resultado no es el esperado.'
