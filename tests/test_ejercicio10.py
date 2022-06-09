976e7e60103edf60f8e9384af3ba4fa4e508aa1c
Santi Chac <santichac3@gmail.com>
2022-06-09 17:02:38 -0300

    Actualice test_ejercicio10.py

--- tests/test_ejercicio10.py
+++ tests/test_ejercicio10.py
@@ -5,7 +5,21 @@ Este es el test correspondiente al archivo 'ejercicio10.py'
 """
 
 def test_es_palindromo():
+    """
+    Este test evalua la función es_palindromo() ingresando una palabra
+    y revisando que sea palindromo.
+    """
     texto = 'neuquen'
     resultado = es_palindromo(texto)
-    assert isinstance(resultado, bool), 'El resultado de la función debe ser de tipo booleano o sea, True o False.'
+    assert isinstance(resultado, bool), 'El resultado de la función debe ser de tipo booleano o sea, True o False. En este caso True.'
     assert resultado == True, 'El resultado no es el esperado.'
+
+def test_no_es_palindromo():
+    """
+    Este test evalua la función es_palindromo() ingresando una palabra
+    y revisando que sea no palindromo.
+    """
+    texto = 'rio negro'
+    resultado = es_palindromo(texto)
+    assert isinstance(resultado, bool), 'El resultado de la función debe ser de tipo booleano o sea, True o False. En este caso False.'
+    assert resultado == False, 'El resultado no es el esperado.'
