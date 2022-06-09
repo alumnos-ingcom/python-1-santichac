d13044ab3862e4a2c6f154d7a79a4ec026309b9c
Santi Chac <santichac3@gmail.com>
2022-06-09 17:10:26 -0300

    Actualice test_ejercicio5.py

--- tests/test_ejercicio5.py
+++ tests/test_ejercicio5.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from src.ejercicio5 import division_lenta
 
 """
@@ -6,7 +7,7 @@ Este es el test correspondiente al archivo 'ejercicio5.py'
 
 def test_division_lenta():
     """
-    Este test evala la función division_lenta() ingresando dos valores y 
+    Este test evalua la función division_lenta() ingresando dos valores y 
     revisando que el resto y el cociente den bien.
     """
     dividendo = 16
@@ -14,3 +15,14 @@ def test_division_lenta():
     resultado = division_lenta(dividendo, divisor)
     assert isinstance(resultado, str), 'El resultado de la división lenta debe dar el cociente y el resto correcto de la división.'
     assert resultado == (f'El cociente es {1} y el resto es {7}'), 'El resultado no es el esperado.'
+
+def test_division_lenta_resto_cero():
+    """
+    Este test evalua la función division_lenta() ingresando dos valores y 
+    revisando que el resto y el cociente den bien.
+    """
+    dividendo = 81
+    divisor = 9
+    resultado = division_lenta(dividendo, divisor)
+    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar el cociente y el resto correcto de la división.'
+    assert resultado == (f'El cociente es {9} y el resto es {0}'), 'El resultado no es el esperado.'
