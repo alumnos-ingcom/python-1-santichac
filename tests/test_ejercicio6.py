7a882504ac83af4f4d371916443aaac7031a779e
santichac <santichac3@gmail.com>
2022-06-09 02:44:17 -0300

    Agregue test_ejercicio6.py

---
+++ tests/test_ejercicio6.py
@@ -0,0 +1,29 @@
+from src.ejercicio6 import ordenar_mayor_a_menor, ordenar_menor_a_mayor
+
+"""
+Este es el test del ejercicio6.py
+"""
+
+def test_ordenar_mayor_a_menor():
+    """
+    Esta test evalua la función ordenar_mayor_a_menor() ingresando tres valores y
+    revisando que el orden sea correcto
+    """
+    uno = 16
+    dos = 3
+    tres = 9
+    resultado = ordenar_mayor_a_menor(uno, dos, tres)
+    assert isinstance(resultado, tuple), 'El resultado debe ser una tupla que se ordene de mayor a menor.'
+    assert resultado == (16, 9, 3), 'El resultado no es el esperado.'
+
+def test_ordenar_menor_a_mayor():
+    """
+    Esta test evalua la función ordenar_menor_a_mayor() ingresando tres valores y
+    revisando que el orden sea correcto
+    """
+    uno = 16
+    dos = 3
+    tres = 9
+    resultado = ordenar_menor_a_mayor(uno, dos, tres)
+    assert isinstance(resultado, tuple), 'El resultado debe ser una tupla que se ordene de menor a mayor.'
+    assert resultado == (3, 9, 16), 'El resultado no es el esperado.'
