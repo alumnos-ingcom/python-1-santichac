e5e7422b855a3bfcb433511374ae558a4e465891
Santi Chac <santichac3@gmail.com>
2022-06-09 16:18:10 -0300

    Agregue ejercicio4.py

---
+++ src/ejercicio4.py
@@ -0,0 +1,53 @@
+################
+# Santiago Julián Chacón - @santichac
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+Escribir una función que haga la suma entre dos números enteros sin hacer
+la operación de manera directa. Esto quiere decir que para hacer la suma entre 4 y 3,
+las operaciones resultantes deberán ser 4+1+1+1.
+
+La funcion debe ser capaz de sumar cualquier numero entero positivo y negativo.
+"""
+
+def signo(numero):
+    """
+    Esta función indica si el numero ingresado
+    es positivo, negativo o cero.
+    """
+    if numero + numero > numero:
+        su_signo = 1
+    elif numero + numero < numero:
+        su_signo = -1
+    else:
+        su_signo = 0
+    return su_signo
+
+def suma_lenta(numero, otro_numero):
+    """
+    Esta función hace una suma de 1 en 1,
+    por ejemplo 4 + 3 aca seria 4 + 1 + 1 + 1.
+    """
+    sgn = signo(otro_numero)
+    suma = numero
+    if sgn == 1:
+        for n in range(otro_numero):
+            suma += 1
+    elif sgn == -1:
+        for n in range(abs(otro_numero)):
+            suma -= 1
+    return suma
+
+def principal():
+    """
+    Esta función es la que se encarga de la parte
+    'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida).
+    """
+    numero = int(input('Ingrese un número: '))
+    otro_numero = int(input('Ingrese el número que le quiera sumar: '))
+    print(suma_lenta(numero, otro_numero))
+
+if __name__ == "__main__":
+    principal()
