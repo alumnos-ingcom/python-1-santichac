d692799ce67c70325158807c5e27e3a25c0ea55c
santichac <santichac3@gmail.com>
2022-06-12 12:50:05 -0300

    Actualice test_ejercicio1.py

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -9,37 +9,37 @@ def test_convertir_a_fahrenheit_positivos():
     Este test evalua la funcion convertir_a_fahrrenheit() ingresando un valor y
     revisando que el resultado sea el numero esperado y de tipo Float
     """
-    centigrados = 31
+    centigrados = 9
     resultado = convertir_a_fahrenheit(centigrados)
     assert isinstance(resultado, float), "El resultado de la conversión debe ser de tipo float o sea, que sea un número con coma."
-    assert resultado == 87.8, "El resultado no es el esperado"
+    assert resultado == 48.2, "El resultado no es el esperado"
     
 def test_convertir_a_fahrenheit_negativos():
     """
     Este test evalua la funcion convertir_a_fahrrenheit() ingresando un valor y
     revisando que el resultado sea el numero esperado y de tipo Float
     """
-    centigrados = -10
+    centigrados = -16
     resultado = convertir_a_fahrenheit(centigrados)
     assert isinstance(resultado, float), "El resultado de la conversión debe ser de tipo float o sea, que sea un número con coma."
-    assert resultado == 14, "El resultado no es el esperado"
+    assert resultado == 3.1999999999999993, "El resultado no es el esperado"
 
 def test_convertir_a_centigrados_positivos():
     """
     Este test evalua la funcion convertir_a_centigrados() ingresando un valor y
     revisando que el resultado sea el numero esperado y de tipo Float
     """
-    fahrenheit = 87.8
+    fahrenheit = 10
     resultado = convertir_a_centigrados(fahrenheit)
     assert isinstance(resultado, float), "El resultado de la conversión debe ser de tipo float o sea, que sea un número con coma."
-    assert resultado == 31, "El resultado no es el esperado"
+    assert resultado == -12.222222222222221, "El resultado no es el esperado"
     
 def test_convertir_a_centigrados_negativos():
     """
     Este test evalua la funcion convertir_a_centigrados() ingresando un valor y
     revisando que el resultado sea el numero esperado y de tipo Float
     """
-    fahrenheit = -5
+    fahrenheit = -3
     resultado = convertir_a_centigrados(fahrenheit)
     assert isinstance(resultado, float), "El resultado de la conversión debe ser de tipo float o sea, que sea un número con coma."
-    assert resultado == -20.555555555555557, "El resultado no es el esperado"
+    assert resultado == -19.444444444444443, "El resultado no es el esperado"
