97331025dd0a6862887e5e610eb387fc75e398af
Santi Chac <santichac3@gmail.com>
2022-06-16 19:08:55 -0300

    Corregi ejercicio2.py

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -9,14 +9,19 @@ si el mismo es positivo, negativo o cero utilizando
 sumas y restas.
 """
 def signo(numero):
-    """Esta funcion indica si el numero ingresado
-    es positivo, negativo o cero"""
+    """
+    Esta funcion indica si el numero ingresado
+    es positivo, negativo o cero.
+    Pre: numero es un número entero o con coma, positivo o negativo
+    Post: la función signo devolvera si el número ingresado es positivo devuelve 1,
+    si es negativo devuelve -1 y si es cero devuelve 0.
+    """
     if numero + numero > 0:
-        el_signo = 'Es positivo'
+        el_signo = 1
     elif numero + numero < 0:
-        el_signo = 'Es negativo'
+        el_signo = -1
     else:
-        el_signo = 'Es cero'
+        el_signo = 0
     return el_signo
 
 def principal():
@@ -26,7 +31,8 @@ def principal():
     (La entrada, la llamada al algoritmo y la salida)
     """
     numero = float(input('Ingrese un numero:'))
-    print(signo(numero))
+    resultado = signo(numero)
+    print(resultado)
 
 if __name__ == "__main__":
     principal()
