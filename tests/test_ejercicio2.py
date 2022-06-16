09f0c6cc333f5869d66c4580bf5f69b1fe9644d7
Santi Chac <santichac3@gmail.com>
2022-06-16 19:08:31 -0300

    Corregi test_ejercicio2.py

--- tests/test_ejercicio2.py
+++ tests/test_ejercicio2.py
@@ -16,8 +16,8 @@ def test_signo_positivo():
     """
     numero = 9.3
     resultado = signo(numero)
-    assert isinstance(resultado, str), 'El resultado debe ser de tipo float o sea, que sea un número con coma.'
-    assert resultado == 'Es positivo', 'El resultado no es el esperado'
+    assert isinstance(resultado, int), 'El resultado debe ser de tipo float o sea, que sea un número con coma.'
+    assert resultado == 1, 'El resultado no es el esperado'
 
 def test_signo_negativo():
     """
@@ -26,8 +26,8 @@ def test_signo_negativo():
     """
     numero = -9.3
     resultado = signo(numero)
-    assert isinstance(resultado, str), 'El resultado debe ser de tipo float o sea, que sea un número con coma.'
-    assert resultado == 'Es negativo', 'El resultado no es el esperado.'
+    assert isinstance(resultado, int), 'El resultado debe ser de tipo float o sea, que sea un número con coma.'
+    assert resultado == -1, 'El resultado no es el esperado.'
 
 def test_signo_cero():
     """
@@ -36,5 +36,5 @@ def test_signo_cero():
     """
     numero = 0
     resultado = signo(numero)
-    assert isinstance(resultado, str),  'El resultado debe ser de tipo float o sea, que sea un número con coma.'
-    assert resultado == 'Es cero', 'El resultado no es el esperado.'
+    assert isinstance(resultado, int),  'El resultado debe ser de tipo float o sea, que sea un número con coma.'
+    assert resultado == 0, 'El resultado no es el esperado.'
