79f2fd5fe7d543a9458139f9f1b2d699e8b6a3bc
Santi Chac <santichac3@gmail.com>
2022-06-16 20:09:23 -0300

    Corregi ejercicio9.py

--- src/ejercicio9.py
+++ src/ejercicio9.py
@@ -11,6 +11,9 @@ factores primos de un numero entero positivo.
 def factores_primos(numero):
     """
     Esta función saca los factores primos de un número
+    Pre: numero es un número entero positivo.
+    Post: la función devuelve los factores primos del número
+    previamente ingresado, en una tupla.
     """
     primos = [ ]
     for primos_res in range(2, numero + 1):
@@ -26,8 +29,8 @@ def principal():
     (La entrada, la llamada al algoritmo y la salida)
     """
     numero = int(input('Ingrese un número para sacar los factores primos: '))
-    print ('Los factores primos de', numero, 'son:', factores_primos(numero))
+    resultado = factores_primos(numero)
+    print (f'Los factores primos de {numero} son: {resultado}')
 
 if __name__ == "__main__":
     principal()
-
