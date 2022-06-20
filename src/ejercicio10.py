db66b9d4bc83060721e62f0684fc49584d85457e
Santi Chac <santichac3@gmail.com>
2022-06-20 17:29:28 -0300

    Corregi ejercicio10.py

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -13,27 +13,31 @@ que de izquierda a derecha.
 def es_palindromo(texto):
     """
     Esta función verifica si una palabra es palindromo o no.
+    Pre: texto es una palabra que ingresa el usuario y que desea
+    saber si es palindromo o no.
+    Post: la función devolvera True si la palabra es palindromo y False
+    si no es palindromo.
     """
-    texto = texto.lower()
-    texto = texto.replace(' ', '')
-    longitud = len(texto)
-    if longitud % 2 == 0:
-        izquierda = texto[:longitud // 2]
-        derecha = texto[longitud // 2:]
+    texto_invertido = ""
+    for l in texto:
+        texto_invertido = l + texto_invertido
+    if texto == texto_invertido:
+        texto_es_palindromo = True
     else:
-        izquierda = texto[:longitud // 2]
-        derecha = texto[longitud // 2 + 1:]
-
-    return izquierda == derecha[::-1]
+        texto_es_palindromo = False
+    return texto_es_palindromo
 
 def principal():
     """
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
-    frase_palabra = input('Ingrese una frase o una palabra:')
-    print(es_palindromo(frase_palabra))
+    frase_palabra = input('Ingrese una frase o una palabra: ')
+    resultado = es_palindromo(frase_palabra)
+    if resultado:
+        print(f'{resultado}, o sea la palabra es palindromo.')
+    else:
+        print(f'{resultado}, o sea la palabra no es palindromo.')
 
 if __name__ == "__main__":
     principal()
-
