cf23856646a9dcb2a54893b128f58c7ab2d19508
santichac <santichac3@gmail.com>
2022-06-21 18:42:24 -0300

    Corregi test_ejercicio5.py

--- tests/test_ejercicio5.py
+++ tests/test_ejercicio5.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 ################
 # Santiago Julián Chacón - @santichac
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-=======
->>>>>>> efb2b8f82b1b7c6286f2e0842da0da9ff5b06228
 from src.ejercicio5 import division_lenta
 
 """
@@ -20,16 +17,39 @@ def test_division_lenta():
     dividendo = 16
     divisor = 9
     resultado = division_lenta(dividendo, divisor)
-    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar el cociente y el resto correcto de la división.'
+    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar 1 de cociente y 7 de resto.'
     assert resultado == (f'El cociente es {1} y el resto es {7}'), 'El resultado no es el esperado.'
 
-def test_division_lenta_resto_cero():
+def test_division_lenta_primero_negativo():
     """
     Este test evalua la función division_lenta() ingresando dos valores y 
     revisando que el resto y el cociente den bien.
     """
-    dividendo = 81
+    dividendo = -81
     divisor = 9
     resultado = division_lenta(dividendo, divisor)
-    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar el cociente y el resto correcto de la división.'
-    assert resultado == (f'El cociente es {9} y el resto es {0}'), 'El resultado no es el esperado.'
+    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar -9 de cociente y 0 de resto.'
+    assert resultado == (f'El cociente es {-9} y el resto es {0}'), 'El resultado no es el esperado.'
+
+def test_division_lenta_ambos_negativos():
+    """
+    Este test evalua la función division_lenta() ingresando dos valores y 
+    revisando que el resto y el cociente den bien.
+    """
+    dividendo = -17
+    divisor = -6
+    resultado = division_lenta(dividendo, divisor)
+    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar 2 de cociente y -5 de resto.'
+    assert resultado == (f'El cociente es {2} y el resto es {-5}'), 'El resultado no es el esperado.'
+
+def test_division_lenta_segundo_negativo():
+    """
+    Este test evalua la función division_lenta() ingresando dos valores y 
+    revisando que el resto y el cociente den bien.
+    """
+    dividendo = 52
+    divisor = -3
+    resultado = division_lenta(dividendo, divisor)
+    assert isinstance(resultado, str), 'El resultado de la división lenta debe dar -17 de cociente y 1 de resto.'
+    assert resultado == (f'El cociente es {-17} y el resto es {1}'), 'El resultado no es el esperado.'
+    
